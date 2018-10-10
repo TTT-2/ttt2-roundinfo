b6086151cf22db23fdf37dca9571afaceeeba637
Alf21 <4lf-mueller@gmx.de>
2018-10-10 23:16:31 +0200

    Compatibility with TTT2 v.0.3.2b
    Beautified

--- lua/autorun/rv_init.lua
+++ lua/autorun/rv_init.lua
@@ -1,94 +1,94 @@
 if SERVER then
 	AddCSLuaFile()
-    
-    util.AddNetworkString("tttRsTellPre")
-    util.AddNetworkString("tttRsTellPost")
-    util.AddNetworkString("tttRsDeathNotify")
-else
-    hook.Add("Initialize", "TTTInitRS", function()
-        LANG.AddToLanguage("English", "ttt_rs_preText", "There are {traits} traitors, {innos} innocents and {specs} spectators this round.")
-        LANG.AddToLanguage("English", "ttt_rs_postText", "The role distribution this round:")
-        LANG.AddToLanguage("English", "ttt_rs_killText", "You were killed by {killer}. Role: {role}.")
-        LANG.AddToLanguage("English", "ttt_rs_suicideText", "You were killed by someone called yourself...")
-        LANG.AddToLanguage("English", "ttt_rs_worldKillText", "You were killed by the world.")
-        
-        LANG.AddToLanguage("Deutsch", "ttt_rs_preText", "Es gibt {traits} Verräter, {innos} Unschuldige und {specs} Zuschauer diese Runde.")
-        LANG.AddToLanguage("Deutsch", "ttt_rs_postText", "Die Rollenverteilung diese Runde:")
-        LANG.AddToLanguage("Deutsch", "ttt_rs_killText", "Du wurdest von {killer} getötet. Rolle: {role}.")
-        LANG.AddToLanguage("Deutsch", "ttt_rs_suicideText", "Du hast dich selbst getötet!")
-        LANG.AddToLanguage("Deutsch", "ttt_rs_worldKillText", "Du wurdest von der Welt getötet.")
-    end)
-    
-	net.Receive("tttRsTellPre", function(len)
-        local PT = LANG.GetParamTranslation
-		local roles = net.ReadTable()
-        local spectators = net.ReadUInt(9)
 
-        local txt = PT("ttt_rs_preText", {traits = roles[ROLE_TRAITOR], innos = roles[ROLE_INNOCENT], specs = spectators})
-        
+	util.AddNetworkString("tttRsTellPre")
+	util.AddNetworkString("tttRsTellPost")
+	util.AddNetworkString("tttRsDeathNotify")
+else
+	hook.Add("Initialize", "TTTInitRS", function()
+		LANG.AddToLanguage("English", "ttt_rs_preText", "There are {traits} traitors, {innos} innocents and {specs} spectators this round.")
+		LANG.AddToLanguage("English", "ttt_rs_postText", "The role distribution this round:")
+		LANG.AddToLanguage("English", "ttt_rs_killText", "You were killed by {killer}. Role: {role}.")
+		LANG.AddToLanguage("English", "ttt_rs_suicideText", "You were killed by someone called yourself...")
+		LANG.AddToLanguage("English", "ttt_rs_worldKillText", "You were killed by the world.")
+
+		LANG.AddToLanguage("Deutsch", "ttt_rs_preText", "Es gibt {traits} Verräter, {innos} Unschuldige und {specs} Zuschauer diese Runde.")
+		LANG.AddToLanguage("Deutsch", "ttt_rs_postText", "Die Rollenverteilung diese Runde:")
+		LANG.AddToLanguage("Deutsch", "ttt_rs_killText", "Du wurdest von {killer} getötet. Rolle: {role}.")
+		LANG.AddToLanguage("Deutsch", "ttt_rs_suicideText", "Du hast dich selbst getötet!")
+		LANG.AddToLanguage("Deutsch", "ttt_rs_worldKillText", "Du wurdest von der Welt getötet.")
+	end)
+
+	net.Receive("tttRsTellPre", function(len)
+		local PT = LANG.GetParamTranslation
+		local roles = net.ReadTable()
+		local spectators = net.ReadUInt(9)
+
+		local txt = PT("ttt_rs_preText", {traits = roles[ROLE_TRAITOR], innos = roles[ROLE_INNOCENT], specs = spectators})
+
 		chat.AddText(Color(255, 255, 0), txt)
 	end)
-    
-    net.Receive("tttRsDeathNotify", function(len)
-        local T = LANG.GetTranslation
-        local PT = LANG.GetParamTranslation
-        
-        local default = Color(205, 155, 0, 255)
-        local killerType = net.ReadUInt(4)
 
-        if killerType == 1 then
-            local role = net.ReadUInt(ROLE_BITS) + 1
-            local killer = net.ReadEntity()
-            
-            local rolename = "unknown"
-            
-            if not ROLES then
-                if role == ROLE_INNOCENT then
-                    rolename = T("innocent")
-                elseif role == ROLE_TRAITOR then
-                    rolename = T("traitor")
-                elseif role == ROLE_DETECTIVE then
-                    rolename = T("detective")
-                end
-            else
-                rolename = T(GetRoleByIndex(role).name)
-            end
-            
-            chat.AddText(default, PT("ttt_rs_killText", {killer = killer:Nick(), role = rolename}))
-        elseif killerType == 2 then
-            chat.AddText(default, T("ttt_rs_suicideText"))
-        elseif killerType == 3 then
-            chat.AddText(default, T("ttt_rs_worldKillText"))
-        end
-    end)
+	net.Receive("tttRsDeathNotify", function(len)
+		local T = LANG.GetTranslation
+		local PT = LANG.GetParamTranslation
+
+		local default = Color(205, 155, 0, 255)
+		local killerType = net.ReadUInt(4)
+
+		if killerType == 1 then
+			local role = net.ReadUInt(ROLE_BITS) + 1
+			local killer = net.ReadEntity()
+
+			local rolename = "unknown"
+
+			if not ROLES then
+				if role == ROLE_INNOCENT then
+					rolename = T("innocent")
+				elseif role == ROLE_TRAITOR then
+					rolename = T("traitor")
+				elseif role == ROLE_DETECTIVE then
+					rolename = T("detective")
+				end
+			else
+				rolename = T(GetRoleByIndex(role).name)
+			end
+
+			chat.AddText(default, PT("ttt_rs_killText", {killer = killer:Nick(), role = rolename}))
+		elseif killerType == 2 then
+			chat.AddText(default, T("ttt_rs_suicideText"))
+		elseif killerType == 3 then
+			chat.AddText(default, T("ttt_rs_worldKillText"))
+		end
+	end)
 
 	net.Receive("tttRsTellPost", function(len)
-        local T = LANG.GetTranslation
-    
+		local T = LANG.GetTranslation
+
 		local rolesnames = net.ReadTable()
 
-        local txt = T("ttt_rs_postText")
-        
+		local txt = T("ttt_rs_postText")
+
 		chat.AddText(Color(255, 255, 0), txt)
-        
-        for k, v in pairs(rolesnames) do
-            if v then
-                if not ROLES then
-                    if k == ROLE_INNOCENT then
-                        txt = (T("innocent") .. ": " .. v)
-                    elseif k == ROLE_TRAITOR then
-                        txt = (T("traitor") .. ": " .. v)
-                    elseif K == ROLE_DETECTIVE then
-                        txt = (T("detective") .. ": " .. v)
-                    end
-                else
-                    local rd = GetRoleByIndex(k)
-                    
-                    txt = (T(rd.name) .. ": " .. v)
-                end
-            end
-        
-            chat.AddText(Color(255, 255, 0), txt)
-        end
+
+		for k, v in pairs(rolesnames) do
+			if v then
+				if not TTT2 then
+					if k == ROLE_INNOCENT then
+						txt = (T("innocent") .. ": " .. v)
+					elseif k == ROLE_TRAITOR then
+						txt = (T("traitor") .. ": " .. v)
+					elseif k == ROLE_DETECTIVE then
+						txt = (T("detective") .. ": " .. v)
+					end
+				else
+					local rd = GetRoleByIndex(k)
+
+					txt = (T(rd.name) .. ": " .. v)
+				end
+			end
+
+			chat.AddText(Color(255, 255, 0), txt)
+		end
 	end)
 end
--- lua/autorun/server/tttchatpeople.lua
+++ lua/autorun/server/tttchatpeople.lua
@@ -1,91 +1,85 @@
 function TellRoles()
-    local rolesnames = {}
-    local roles = {}
-    local spectators = 0
-    
-    if not ROLES then
+	local rolesnames = {}
+	local roles = {}
+	local spectators = 0
+
+	if not TTT2 then
 		roles[ROLE_INNOCENT] = 0
 		roles[ROLE_TRAITOR] = 0
 		roles[ROLE_DETECTIVE] = 0 -- not needed
-	
-        rolesnames[ROLE_INNOCENT] = {}
-        rolesnames[ROLE_TRAITOR] = {}
-        rolesnames[ROLE_DETECTIVE] = {}
-    else
-        for _, v in pairs(ROLES) do
+
+		rolesnames[ROLE_INNOCENT] = {}
+		rolesnames[ROLE_TRAITOR] = {}
+		rolesnames[ROLE_DETECTIVE] = {}
+	else
+		for _, v in pairs(GetRoles()) do
 			roles[v.index] = 0
-		
-            rolesnames[v.index] = {}
-        end
-    end
 
-    for _, v in pairs(player.GetAll()) do
-        if not (v:Alive() and v:IsTerror()) then
-            spectators = spectators + 1
-        else
-            local role = v:GetRole()
-            
-            table.insert(rolesnames[role], v:Nick())
-            
-            if not ROLES then
-                role = role == ROLE_DETECTIVE and ROLE_INNOCENT or role
-            
-                roles[role] = (roles[role] or 0) + 1
-            else
-                local rd = GetRoleByIndex(role)
-                
-                if rd.team == TEAM_TRAITOR then
-                    roles[ROLE_TRAITOR] = roles[ROLE_TRAITOR] + 1
-                elseif rd.team == TEAM_INNO then
-                    roles[ROLE_INNOCENT] = roles[ROLE_INNOCENT] + 1
-				else
-					roles[role] = (roles[role] or 0) + 1
-                end
-            end
-        end
-    end
-	
+			rolesnames[v.index] = {}
+		end
+	end
+
+	for _, v in ipairs(player.GetAll()) do
+		if not v:Alive() or not v:IsTerror() then
+			spectators = spectators + 1
+		else
+			local role = v:GetRole() -- in TTT2 return BaseRole
+
+			table.insert(rolesnames[role], v:Nick())
+
+			role = role == ROLE_DETECTIVE and ROLE_INNOCENT or role
+
+			roles[role] = (roles[role] or 0) + 1
+		end
+	end
+
 	hook.Run("TTTAModifyRolesTable", roles)
-    
-    rolesnamestext = {}
-    
-    for k, v in pairs(rolesnames) do
-        if v and #v > 0 then
-            rolesnamestext[k] = table.concat(v, ", ")
-        end
-    end
 
-    for _, v in pairs(player.GetAll()) do
-        net.Start("tttRsTellPre")
-        net.WriteTable(roles)
-        net.WriteUInt(spectators, 9)
-        net.Send(v)
-    end
+	rolesnamestext = {}
+
+	for k, v in pairs(rolesnames) do
+		if v and #v > 0 then
+			rolesnamestext[k] = table.concat(v, ", ")
+		end
+	end
+
+	for _, v in ipairs(player.GetAll()) do
+		net.Start("tttRsTellPre")
+		net.WriteTable(roles)
+		net.WriteUInt(spectators, 9)
+		net.Send(v)
+	end
 end
 hook.Add("TTTBeginRound", "TTTChatStats", TellRoles)
 
 function TellKiller(victim, weapon, killer)
-    net.Start("tttRsDeathNotify")
-    
-    if killer:IsPlayer() and killer ~= victim then
-        net.WriteUInt(1, 4)
-        net.WriteUInt(killer:GetRole() - 1, ROLE_BITS)
-        net.WriteEntity(killer)
-    elseif killer:IsPlayer() and killer == victim then
-        net.WriteUInt(2, 4)
-    else
-        net.WriteUInt(3, 4)
-    end
-    
-    net.Send(victim)
+	net.Start("tttRsDeathNotify")
+
+	if killer:IsPlayer() and killer ~= victim then
+		net.WriteUInt(1, 4)
+
+		if TTT2 then
+			net.WriteUInt(killer:GetSubRole(), ROLE_BITS)
+		else
+			net.WriteUInt(killer:GetRole(), 2)
+		end
+
+		net.WriteEntity(killer)
+	elseif killer:IsPlayer() and killer == victim then
+		net.WriteUInt(2, 4)
+	else
+		net.WriteUInt(3, 4)
+	end
+
+	net.Send(victim)
 end
 hook.Add("PlayerDeath", "TTTChatStats", TellKiller)
 
 function TellRolesNames()
-    for _, v in pairs(player.GetAll()) do
-        net.Start("tttRsTellPost")
-        net.WriteTable(rolesnamestext)
-        net.Send(v)
-    end
+	for _, v in pairs(player.GetAll()) do
+		net.Start("tttRsTellPost")
+		net.WriteTable(rolesnamestext)
+		net.Send(v)
+	end
 end
 hook.Add("TTTEndRound", "TTTChatStats", TellRolesNames)
