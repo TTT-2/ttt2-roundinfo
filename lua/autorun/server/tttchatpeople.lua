477e84bae7d3939d3d2769739691b1f02e4ccd88
Alf21 <4lf-mueller@gmx.de>
2018-12-15 23:32:03 +0100

    Fix Alf21/TTT2/#174

--- lua/autorun/server/tttchatpeople.lua
+++ lua/autorun/server/tttchatpeople.lua
@@ -1,5 +1,6 @@
 function TellRoles()
 	if not GetConVar("ttt_rolesetup_tell_pre_roles"):GetBool() then return end
+
 	local rolesnames = {}
 	local roles = {}
 	local spectators = 0
@@ -94,7 +95,8 @@ end
 hook.Add("PlayerDeath", "TTTChatStats", TellKiller)
 
 function TellRolesNames()
-	if not GetConVar("ttt_rolesetup_tell_after_roles"):GetBool() then return end
+	if not GetConVar("ttt_rolesetup_tell_after_roles"):GetBool() or not rolesnamestext then return end
+
 	for _, v in ipairs(player.GetAll()) do
 		local amount = 0
 
