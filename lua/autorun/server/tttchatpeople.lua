9157241d6f7ac3f62f8d8ae55e18851e7cdec5ff
Alf Müller <4lf-mueller@gmx.de>
2019-03-08 20:07:26 +0100

    improvement

--- lua/autorun/server/tttchatpeople.lua
+++ lua/autorun/server/tttchatpeople.lua
@@ -1,5 +1,4 @@
 function TellRoles()
-
 	local rolesnames = {}
 	local rls = {}
 	local spectators = 0
@@ -46,14 +45,8 @@ function TellRoles()
 
 	if not GetConVar("ttt_rolesetup_tell_pre_roles"):GetBool() then return end
 
-	local roles_size = 0
-
-	for _ in pairs(rls) do
-		roles_size = roles_size + 1
-	end
-
 	net.Start("tttRsTellPre")
-	net.WriteUInt(roles_size, ROLE_BITS)
+	net.WriteUInt(table.Count(rls), ROLE_BITS)
 
 	for role, amount in pairs(rls) do
 		net.WriteUInt(role, ROLE_BITS)
@@ -99,14 +92,8 @@ function TellRolesNames()
 	if not GetConVar("ttt_rolesetup_tell_after_roles"):GetBool() or not rolesnamestext then return end
 
 	for _, v in ipairs(player.GetAll()) do
-		local amount = 0
-
-		for _ in pairs(rolesnamestext) do
-			amount = amount + 1
-		end
-
 		net.Start("tttRsTellPost")
-		net.WriteUInt(amount, ROLE_BITS)
+		net.WriteUInt(table.Count(rolesnamestext), ROLE_BITS)
 
 		for role, nicks in pairs(rolesnamestext) do
 			net.WriteUInt(role, ROLE_BITS)
