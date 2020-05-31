304d3f9c0d9e337380770a1db12477354034a6f6
Tim Goll <tim@timgoll.de>
2020-05-31 14:53:25 +0200

    switched to new lang system

---
+++ lua/lang/deutsch/roundinfo.lua
@@ -0,0 +1,18 @@
+L = LANG.GetLanguageTableReference("deutsch")
+
+L["ttt_rs_preText"] = "0%Es gibt %1%{traits} Verräter%0%, %2%{innos} Unschuldige%0% und %3%{specs} Zuschauer%0% diese Runde."
+L["ttt_rs_postText"] = "Die Rollenverteilung diese Runde:"
+L["ttt_rs_killText"] = "0%Du wurdest von %1%{killer}%0% getötet. Rolle: %2%{role}%0%. (Art: {killtype})"
+L["ttt_rs_suicideText"] = "0%Du hast dich selbst getötet! (Art: {killtype})"
+L["ttt_rs_worldKillText"] = "0%Du wurdest von der Welt getötet."
+
+L["ttt_rs_killtype_unknown"] = "unbekannt"
+L["ttt_rs_killtype_propkill"] = "Propkill"
+L["ttt_rs_killtype_falldamage"] = "Fallschaden"
+L["ttt_rs_killtype_firedamage"] = "Feuerschaden"
+L["ttt_rs_killtype_explosion"] = "Explosion"
+L["ttt_rs_killtype_drowned"] = "ertrunken"
+
+L["ttt_rs_you_were_killed"] = "du wurdest getötet von"
+L["ttt_rs_killer_yourself"] = "dir selbst"
+L["ttt_rs_killer_world"] = "Welt"
---
+++ lua/lang/english/roundinfo.lua
@@ -0,0 +1,18 @@
+L = LANG.GetLanguageTableReference("english")
+
+L["ttt_rs_preText"] = "0%There are %1%{traits} traitors%0%, %2%{innos} innocents%0% and %3%{specs} spectators%0% this round."
+L["ttt_rs_postText"] = "The role distribution this round:"
+L["ttt_rs_killText"] = "0%You were killed by %1%{killer}%0%. Role: %2%{role}%0%. (type: {killtype})"
+L["ttt_rs_suicideText"] = "0%You were killed by someone called yourself... (type: {killtype})"
+L["ttt_rs_worldKillText"] = "0%You were killed by the world."
+
+L["ttt_rs_killtype_unknown"] = "unknown"
+L["ttt_rs_killtype_propkill"] = "propkill"
+L["ttt_rs_killtype_falldamage"] = "falldamage"
+L["ttt_rs_killtype_firedamage"] = "firedamage"
+L["ttt_rs_killtype_explosion"] = "explosion"
+L["ttt_rs_killtype_drowned"] = "drowned"
+
+L["ttt_rs_you_were_killed"] = "you were killed by"
+L["ttt_rs_killer_yourself"] = "yourself"
+L["ttt_rs_killer_world"] = "world"
