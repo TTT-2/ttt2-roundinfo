2d4ac35fa05522b0239f66201777a41d7dc823c3
Alessandro Levante <62677560+PinoMartirio@users.noreply.github.com>
2020-05-31 14:54:01 +0200

    Created Italian translation (#15)

---
+++ lua/lang/italiano/roundinfo.lua
@@ -0,0 +1,18 @@
+L = LANG.GetLanguageTableReference("italiano")
+
+L["ttt_rs_preText"] = "0%Ci sono %1%{traits} traditori, %2%{innos} innocenti e %3%{specs} spettatori questo round."
+L["ttt_rs_postText"] = "La distribuzione dei ruoli questo round:"
+L["ttt_rs_killText"] = "0%Sei stato ucciso da %1%{killer}%0%. Ruolo: %2%{role}%0%. (morte: {killtype})"
+L["ttt_rs_suicideText"] = "0%Ti sei suicidato... (morte: {killtype})"
+L["ttt_rs_worldKillText"] = "0%Sei stato ucciso dal mondo."
+
+L["ttt_rs_killtype_unknown"] = "sconosciuto"
+L["ttt_rs_killtype_propkill"] = "propkill"
+L["ttt_rs_killtype_falldamage"] = "danno da caduta"
+L["ttt_rs_killtype_firedamage"] = "danno da fuoco"
+L["ttt_rs_killtype_explosion"] = "esplosione"
+L["ttt_rs_killtype_drowned"] = "annegato"
+
+L["ttt_rs_you_were_killed"] = "sei stato ucciso da"
+L["ttt_rs_killer_yourself"] = "te stesso"
+L["ttt_rs_killer_world"] = "mondo"
