1d1aed62a203c97d2b6aada91ba23f86e32afafc
Tekiad <tekiadp@gmail.cm>
2020-09-18 13:16:48 -0300

    Create roundinfo.lua (#16)

---
+++ lua/lang/español/roundinfo.lua
@@ -0,0 +1,18 @@
+L = LANG.GetLanguageTableReference("Español")
+
+L["ttt_rs_preText"] = "0%Hay %1%{traits} traidores%0%, %2%{innos} inocentes%0% y %3%{specs} espectadores%0% en esta ronda."
+L["ttt_rs_postText"] = "La distribución de roles esta ronda:"
+L["ttt_rs_killText"] = "0%Fuiste asesinado por %1%{killer}%0%. Rol: %2%{role}%0%. (tipo: {killtype})"
+L["ttt_rs_suicideText"] = "0%Fuiste asesinado por ti mismo... (tipo: {killtype})"
+L["ttt_rs_worldKillText"] = "0%Fuiste expurgado del mundo."
+
+L["ttt_rs_killtype_unknown"] = "desconocida"
+L["ttt_rs_killtype_propkill"] = "propkill"
+L["ttt_rs_killtype_falldamage"] = "muertoporcaida"
+L["ttt_rs_killtype_firedamage"] = "chamuscado"
+L["ttt_rs_killtype_explosion"] = "explosión"
+L["ttt_rs_killtype_drowned"] = "ahogado"
+
+L["ttt_rs_you_were_killed"] = "fuiste asesinado por"
+L["ttt_rs_killer_yourself"] = "ti mismo"
+L["ttt_rs_killer_world"] = "mundo"
