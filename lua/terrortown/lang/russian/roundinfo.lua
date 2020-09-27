c48a9faa415923214f983aa8cc1a1642acda8314
Blueberryy <36592509+Blueberryy@users.noreply.github.com>
2020-09-27 15:31:32 +0500

    Added russian translation

---
+++ lua/terrortown/lang/russian/roundinfo.lua
@@ -0,0 +1,18 @@
+local L = LANG.GetLanguageTableReference("Русский")
+
+L["ttt_rs_preText"] = "0%Есть %1%{traits} предателей%0%, %2%{innos} невиновных%0% и %3%{specs} наблюдателей%0% в этом раунде."
+L["ttt_rs_postText"] = "Распределение ролей в этом раунде:"
+L["ttt_rs_killText"] = "0%Вы были убиты игроком %1%{killer}%0%. Роль: %2%{role}%0%. (тип: {killtype})"
+L["ttt_rs_suicideText"] = "0%Вы были убиты кем-то или чем-то по имени... (тип: {killtype})"
+L["ttt_rs_worldKillText"] = "0%Вы были убиты миром."
+
+L["ttt_rs_killtype_unknown"] = "неизвестно"
+L["ttt_rs_killtype_propkill"] = "убийство объектом"
+L["ttt_rs_killtype_falldamage"] = "урон от падения"
+L["ttt_rs_killtype_firedamage"] = "урон от огня"
+L["ttt_rs_killtype_explosion"] = "взрыв"
+L["ttt_rs_killtype_drowned"] = "утонул"
+
+L["ttt_rs_you_were_killed"] = "вы были убиты"
+L["ttt_rs_killer_yourself"] = "от самого себя"
+L["ttt_rs_killer_world"] = "миром"
