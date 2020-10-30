e7623b7d28fa6dc2d7a4197e8cc753639c724c44
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-30 21:49:41 +0100

    Create roundinfo.lua (#18)
    french translation :round info

---
+++ lua/terrortown/lang/french/roundinfo.lua
@@ -0,0 +1,18 @@
+local L = LANG.GetLanguageTableReference("french")
+
+L["ttt_rs_preText"] = "0%Il y a %1%{traits} traitres%0%, %2%{innos} innocents%0% et %3%{specs} spectateurs%0% ce round."
+L["ttt_rs_postText"] = "La répartition des rôles du round:"
+L["ttt_rs_killText"] = "0%Vous avez été tué par %1%{killer}%0%. Rôle: %2%{role}%0%. (type: {killtype})"
+L["ttt_rs_suicideText"] = "0%Vous avez été tué par vous-même... (type: {killtype})"
+L["ttt_rs_worldKillText"] = "0%Vous avez été tué par le monde."
+
+L["ttt_rs_killtype_unknown"] = "inconnu"
+L["ttt_rs_killtype_propkill"] = "propkill"
+L["ttt_rs_killtype_falldamage"] = "dégâts de chutes"
+L["ttt_rs_killtype_firedamage"] = "dégâts dus au feu"
+L["ttt_rs_killtype_explosion"] = "explosion"
+L["ttt_rs_killtype_drowned"] = "noyade"
+
+L["ttt_rs_you_were_killed"] = "vous avez été tué par"
+L["ttt_rs_killer_yourself"] = "vous-même"
+L["ttt_rs_killer_world"] = "monde"
