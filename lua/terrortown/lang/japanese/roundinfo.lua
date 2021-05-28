d341dbc316d8729928c7abdd152b00a02db57b40
westooooo <68987820+westooooo@users.noreply.github.com>
2021-05-28 01:15:05 -0900

    Add Japanese translation (#19)

---
+++ lua/terrortown/lang/japanese/roundinfo.lua
@@ -0,0 +1,18 @@
+local L = LANG.GetLanguageTableReference("日本語")
+
+L["ttt_rs_preText"] = "0%このラウンドでは%1%Traitor%0%が{traits}人、%2%Innocent%0%が{innos}人、そして%3%Spectator%0%が{specs}人います。"
+L["ttt_rs_postText"] = "このラウンドの役職の内訳:"
+L["ttt_rs_killText"] = "0%あなたは%1%{killer}%0%により殺されました。役職:%2%{role}%0%. (死因:{killtype})"
+L["ttt_rs_suicideText"] = "0%あなたは自滅してしまいました...(死因：{killtype})"
+L["ttt_rs_worldKillText"] = "0%あなたは世界により殺されました。"
+
+L["ttt_rs_killtype_unknown"] = "不明"
+L["ttt_rs_killtype_propkill"] = "圧死"
+L["ttt_rs_killtype_falldamage"] = "転落死"
+L["ttt_rs_killtype_firedamage"] = "焼死"
+L["ttt_rs_killtype_explosion"] = "爆死"
+L["ttt_rs_killtype_drowned"] = "溺死"
+
+L["ttt_rs_you_were_killed"] = "あなたは死亡しました。殺害者は"
+L["ttt_rs_killer_yourself"] = "あなた自身"
+L["ttt_rs_killer_world"] = "世界"
