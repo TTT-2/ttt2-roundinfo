850517c961b56db53b2b43ecad8fe7b5174a4ee6
sbzl <92261479+sbzlzh@users.noreply.github.com>
2022-07-26 18:33:01 +0800

    Create simplified Chinese Translation (#22)

---
+++ lua/terrortown/lang/zh_hans/roundinfo.lua
@@ -0,0 +1,18 @@
+local L = LANG.GetLanguageTableReference("zh_hans")
+
+L["ttt_rs_preText"] = "0%本轮有 %1%{traits} 名叛徒%0%,%2%{innos} 名无辜者%0% 和 %3%{specs} 名旁观者%0%."
+L["ttt_rs_postText"] = "本轮角色分配:"
+L["ttt_rs_killText"] = "0%你被 %1%{killer}%0% 杀死了. 角色: %2%{role}%0%. (类型: {killtype})"
+L["ttt_rs_suicideText"] = "0%你被一个自称为你的人杀死了......(类型: {killtype})"
+L["ttt_rs_worldKillText"] = "0%你被世界杀死了."
+
+L["ttt_rs_killtype_unknown"] = "未知"
+L["ttt_rs_killtype_propkill"] = "物理杀"
+L["ttt_rs_killtype_falldamage"] = "摔死"
+L["ttt_rs_killtype_firedamage"] = "烧死"
+L["ttt_rs_killtype_explosion"] = "爆炸"
+L["ttt_rs_killtype_drowned"] = "淹死"
+
+L["ttt_rs_you_were_killed"] = "你被杀了"
+L["ttt_rs_killer_yourself"] = "你自己"
+L["ttt_rs_killer_world"] = "世界"
