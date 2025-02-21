f37fdf076774f291b41a7a1cc59c1657454771cd
Tora <49911006+NovaDiablox@users.noreply.github.com>
2025-02-21 13:19:35 +0300

    Added Turkish Localization (#27)

---
+++ lua/terrortown/lang/tr/roundinfo.lua
@@ -0,0 +1,29 @@
+local L = LANG.GetLanguageTableReference("tr")
+
+L["ttt_rs_preText"] = "0%Bu raunt %1%{traits} hain%0%, %2%{innos} masum%0% ve %3%{specs} izleyici%0% var."
+L["ttt_rs_postText"] = "Bu raunt rol dağılımı:"
+L["ttt_rs_killText"] = "0% %1%{killer}%0% tarafından öldürülmüştün. Rol: %2%{role}%0%. (tür: {killtype})"
+L["ttt_rs_suicideText"] = "0%Sen diye biri tarafından öldürülmüştün... (tür: {killtype})"
+L["ttt_rs_worldKillText"] = "0%Dünya tarafından öldürülmüştün."
+
+L["ttt_rs_killtype_unknown"] = "bilinmeyen sebep"
+L["ttt_rs_killtype_propkill"] = "nesne ölümü"
+L["ttt_rs_killtype_falldamage"] = "düşme hasarı"
+L["ttt_rs_killtype_firedamage"] = "ateş hasarı"
+L["ttt_rs_killtype_explosion"] = "patlama"
+L["ttt_rs_killtype_drowned"] = "boğulma"
+
+L["ttt_rs_you_were_killed"] = "şu kişi sizi öldürmüştü"
+L["ttt_rs_killer_yourself"] = "kendiniz"
+L["ttt_rs_killer_world"] = "dünya"
+
+L["submenu_server_addons_roundinfo_title"] = "Raunt Bilgisi"
+L["header_addons_roundinfo"] = "Raunt Bilgisi Ayarları"
+L["header_addons_roundinfo_chat"] = "Raunt Bilgisi: Sohbet Mesajları"
+L["header_addons_roundinfo_popup"] = "Raunt Bilgisi: Açılır Pencere"
+
+L["label_roundinfo_pre_announce_distribution"] = "Dağılımı Raunt Öncesi Duyur"
+L["label_roundinfo_post_announce_distribution"] = "Dağılımı Raunt Sonrası Duyur"
+L["label_roundinfo_announce_killer"] = "Katili Duyur"
+L["label_roundinfo_popup_killer"] = "Katil Bilgisini Göster"
+L["label_roundinfo_popup_killer_time"] = "Katil Bilgi Süresini Göster"
